330c8b82bd988e2c9a24017516f9e1669376f7cb
Gokul Suresh <gokul.suresh24@gmail.com>
2016-06-14 09:56:00 +0530

    began working on main

--- chip8.cpp
+++ chip8.cpp
@@ -1,2 +1,29 @@
-#include "gs8.h"	/* the cpu core implementation */
+#include "chip8.h"	/* the cpu core implementation */
 
+chip8 myChip8;
+
+int main(int argc, char ** argv) {
+
+	// Set up the render system and register input callbacks
+	setupGraphics();
+	setupInput();
+
+	// Initialize the Chip8 system and load the game into memory
+	myChip8.initialize();
+	myChip8.loadGame("pong");
+}
+
+
+void chip8::initialize() {
+	pc = 0x200;	// the program counter starts at 0x200
+	opcode = 0;	// reset the opcode
+	I = 0;		// reset the index register
+	sp = 0;		// reset the stack pointer
+
+
+
+	// load the fontset
+	for (int i = 0; i < 80; i++) {
+		memory[i] = chip8_fontset[i];
+	}
+}
