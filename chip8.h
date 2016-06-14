c50a3a67a143450a393d5ad8794c75416c5a8d62
Gokul Suresh <gokul.suresh24@gmail.com>
2016-06-14 09:55:24 +0530

    Outlined class definition in header file

--- chip8.h
+++ chip8.h
@@ -8,6 +8,15 @@ class chip8 {
 	unsigned char V[16];		/* holds the 15 cpu registers + carry flag */
 	unsigned short I;			/* the Index register */
 	unsigned short pc;			/* the program counter */
+	unsigned char gfx[64 * 32];	/* graphics array */
+	unsigned char delay_timer;	/* the delay timer */
+	unsigned char sound_timer;	/* the sound timer */
+
+	unsigned short stack[16];	/* the RTS */
+	unsigned short sp;			/* the stack pointer */
+
+	unsigned short key[16];		/* used to implement the hex based keypad
+								for chip 8 */
 
 public:
 	void initialize();
