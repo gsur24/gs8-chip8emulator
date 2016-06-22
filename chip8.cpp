7b66a2b3d1d01a651a61d439f345741feaf08f53
Gokul Suresh <gokul.suresh24@gmail.com>
2016-06-22 11:29:38 +0530

    Begin working on SDL graphics implementation

--- chip8.cpp
+++ chip8.cpp
@@ -1,7 +1,8 @@
 #include "chip8.h"	/* the cpu core implementation */
-#include <SDL2/SDL.h>
 #include <iostream>
 #include <stdio.h>
+#include <SDL.h>
+
 using namespace std;
 
 chip8 myChip8;
@@ -26,6 +27,7 @@ void chip8::loadGame(const char * filename) {
 	long bufferSize;
 	FILE * pFile;
 
+	// opening the file in read-only binary mode
 	pFile = fopen (filename, "rb");
 
 	if (pFile == NULL) {
@@ -78,8 +80,9 @@ void chip8::emulateCycle() {
 				break;
 				
 				case 0x00EE: // 00EE: return from subroutine
-					sp--;
+					--sp;
 					pc = stack[sp];
+					pc += 2;
 
 					if (debug)
 						cerr << opcode << ": returning from subroutine\n";
@@ -90,6 +93,7 @@ void chip8::emulateCycle() {
 					cerr << opcode << " not implemented\n";
 					exit(0);
 			}
+		break;	
 
 		case 0x1000: // 1NNN: jumps to address NNN
 			pc = opcode & 0x0FFF;
@@ -109,11 +113,10 @@ void chip8::emulateCycle() {
 		break;
 
 		case 0x3000: { // 3XNN: skips the next instruction if VX equals NN
-			int x = (opcode & 0x0FF0) >> 8;
-			unsigned char nn = (opcode & 0x00FF);
+			int x = (opcode & 0x0F00) >> 8;
 
 			// checking if V[X] == NN
-			if (V[x] == nn) { 
+			if (V[x] == (opcode & 0x00FF)) { 
 				if (debug)
 					cerr << opcode << ": skipping next instruction\n";
 
@@ -128,10 +131,11 @@ void chip8::emulateCycle() {
 				pc += 2;
 			}
 
-		break;
 		}
+		break;
 
 		case 0x4000: // 4XNN: skips the next instruction if VX doesn't equal NN
+
 			if(V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
 				pc += 4;
 				if (debug)
@@ -168,24 +172,23 @@ void chip8::emulateCycle() {
 
 			V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
 			pc += 2;
-			break;
+		break;
 		
 		case 0x7000: // 7XNN: adds NN to VX
 			
 			if (debug)
 				cerr << opcode << ": adding NN to VX\n";
 
-			V[(opcode & 0x0F00) >> 8] = (V[(opcode & 0x0F00) >> 8] + (opcode & 
-			0x00FF)) & 0xFF; 
+			V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;	
 
 			pc += 2;
-			break;
+		break;
 
 		case 0x8000: // contains more information in last nibble
 			switch (opcode & 0x000F) {
 				
 				case 0x0000: // 0x8XY0: sets VX to the value of VY
-					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 8];
+					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
 					pc += 2;
 
 					if (debug)
@@ -222,14 +225,43 @@ void chip8::emulateCycle() {
 					else 
 						V[0xF] = 0;		// carry set to 0
 					V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
-					pc +=2;
+					pc += 2;
 				break;		
 
+				case 0x0005: // 8XY5: VY is subtracted from VX
+					if(V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
+						V[0xF] = 0;
+					else
+						V[0xF] = 1;
+					V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+					if (debug)
+						cerr << opcode << ": subtracting VY from VX\n";
+					pc += 2;	
+				break;
+
+				case 0x0006: // 8XY6: shifts VX right by one
+					V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1;
+					V[(opcode & 0x0F00) >> 8] >>= 1;
+					pc += 2;
+					if (debug)
+						cerr << opcode << ": shifting VX right by one\n";
+				break;
+
+				case 0x0007: // 8XY7: Sets VX to VY minus VX
+					if(V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])
+						V[0xF] = 0;
+					else
+						V[0xF] = 1;
+					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+					if (debug)
+						cerr << opcode << ": V[X] = V[Y] - V[X]\n";
+				break;		
+					
 				default:
 					cerr << "Unknown opcode: " << opcode << "\n";
 					exit(0);
-				break;
 			}
+		break;	
 
 		case 0xA000: // ANNN: sets I to the address NNN
 			if (debug)
@@ -237,7 +269,14 @@ void chip8::emulateCycle() {
 
 			I = opcode & 0x0FFF;
 			pc += 2;
-			break;
+		break;
+
+		case 0xC000: // CXNN: Sets VX to a random number and NN
+			V[(opcode & 0x0F00) >> 8] = (rand() % 0xFF) & (opcode & 0x00FF);
+			pc += 2;
+			if (debug)
+				cerr << opcode << ": setting VX to a random number\n";
+		break;		
 		
 		case 0xD000:  { // DXYN: Draw a sprite (X, Y) size (8, N) at location I
 			unsigned short x = V[(opcode & 0x0F00) >> 8];	// x-coordinate
@@ -263,22 +302,42 @@ void chip8::emulateCycle() {
 			// screen now needs to be redrawn
 			drawFlag = true;
 			pc += 2;
-			break;
+		break;
 		}
+
+		case 0xE000:	// multi-case opcode
+			switch (opcode & 0x00FF) {
+
+				// TODO
+			}
 
 		case 0xF000:	// multi-case opcode
 			
 			switch(opcode & 0x00FF) {
 
-				case 0x0029: {	// sets I to the location of the sprite
+				case 0x0007:	// FX07: sets V[x] to the value of delay timer
+					V[(opcode & 0x0F00) >> 8] = delay_timer;
+					if (debug)
+						cerr << opcode << ": setting V[x] to value of delay timer\n";
+					pc += 2;
+				break;		
+
+				case 0x0015:	// FX15: sets the delay timer to V[x]
+					delay_timer = V[(opcode & 0x0F00) >> 8];
+					if (debug)
+						cerr << opcode << ": setting delay timer to V[x]\n";
+					pc += 2;
+				break;	
+
+				case 0x0029: {	// FX29: sets I to the location of the sprite
 					int x = (opcode & 0x0F00) >> 8;
 					unsigned char character = V[x];
 					I = (0x050 + (character*5));
 					if (debug) 
 						cerr << opcode << ": setting I to location of sprite\n";
 					pc += 2;
-					break;
 				}
+				break;
 
 				case 0x0033: {	// Stores a binary coded decimal value VX in I
 								// I+1, and I+2
@@ -296,9 +355,8 @@ void chip8::emulateCycle() {
 						<< " in I, I+1, I+2\n";
 					}
 					pc += 2;
-					break;
-					
 				}
+				break;
 
 				case 0x0065: {	// fills V0 to VX with values from I
 					int x = (opcode & 0x0F00) >> 8;
@@ -312,27 +370,37 @@ void chip8::emulateCycle() {
 					}
 
 					pc += 2;
-					break;
 				}
+				break;
 
-			break;	
-
-
+				default:
+					cerr << "Unknown opcode: " << opcode << "\n";
+					exit(0);
 
 			}
+		break;
 
 		default:
 			cerr << "Unknown opcode: " << opcode << "\n";
 			exit(0);
 	}
 
-	// execute opcode
+	// update timers
+	if (delay_timer > 0) {
+		--delay_timer;
+	}
+
+	if (sound_timer > 0) {
+		if (sound_timer == 1)
+			cout << "BEEP!\n";
+		--sound_timer;
+	}
 }
 
 int main(int argc, char ** argv) {
 	char option;
 
-	chip8::Set_Debug_Off();
+	chip8::Set_Debug_On();
 
 	// checking if debug mode (x) was passed as a command line parameter
 	while ((option = getopt (argc, argv, "x")) != EOF) {
@@ -342,16 +410,53 @@ int main(int argc, char ** argv) {
 					  break;
 		}
 	}
+    
+    // The SDL window
+    SDL_Window * window = NULL;
+    
+    // The surface contained by the window
+    SDL_Surface * screenSurface = NULL;
+    
+    // Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    
+    else {
+        // Create the window
+        window = SDL_CreateWindow("GS8 Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
+        
+        if (window == NULL) {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        
+        else {
+            // Get the window surface
+            screenSurface = SDL_GetWindowSurface(window);
+            
+            // Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+            
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
 	
 	// initialize the chip
 	myChip8.initialize();
-	myChip8.loadGame("PONG");
+	myChip8.loadGame("PONG2");
 	
 	// emulation loop
 	while (true) {
 		myChip8.emulateCycle();
 
 	}
+    
+    
+    
 
 }
 
